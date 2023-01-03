8bf176db3f3f528598c7505df1aa3a9108fb5f51
Soyka <petr.emelianov@icloud.com>
2023-01-03 15:50:11 +0200

    upd

--- Game2048.java
+++ Game2048.java
@@ -1,12 +1,16 @@
-package com.javarush.games.game2048;
-
 import com.javarush.engine.cell.*;
 
 public class Game2048 extends Game {
     private static final int SIDE = 4;
+    private int[][] gameField = new int[SIDE][SIDE];
 
     @Override
     public void initialize() {
         setScreenSize(SIDE, SIDE);
+        createGame();
+    }
+
+    private void createGame() {
+
     }
 }
