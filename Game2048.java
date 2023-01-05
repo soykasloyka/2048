323fe7d299e423123a21d5e4fe8f90633f9977eb
Soyka <petr.emelianov@icloud.com>
2023-01-05 11:15:41 +0200

    screen settings

--- Game2048.java
+++ Game2048.java
@@ -10,9 +10,18 @@ public class Game2048 extends Game {
     public void initialize() {
         setScreenSize(SIDE, SIDE);
         createGame();
+        drawScene();
     }
 
     private void createGame() {
 
+    }
+
+    private void drawScene() {
+        for (int y = 0; y < SIDE; y++) {
+            for (int x = 0; x < SIDE; x++) {
+                setCellColor(x, y, Color.GREEN);
+            }
+        }
     }
 }
