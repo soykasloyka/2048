fa2ac3cf0ea73ffafb69a584af0592576e362b2e
Soyka <petr.emelianov@icloud.com>
2023-01-09 12:04:50 +0200

    colors

--- Game2048.java
+++ Game2048.java
@@ -31,10 +31,47 @@ public class Game2048 extends Game {
         while (!isCreated);
     }
 
+    private void setCellColoredNumber(int x, int y, int value) {
+        Color color = getColorByValue(value);
+        String str = value > 0 ? "" + value : "";
+        setCellValueEx(x, y, color, str);
+    }
+
+    private Color getColorByValue(int value) {
+        switch (value) {
+            case 0:
+                return Color.WHITE;
+            case 2:
+                return Color.PLUM;
+            case 4:
+                return Color.SLATEBLUE;
+            case 8:
+                return Color.DODGERBLUE;
+            case 16:
+                return Color.DARKTURQUOISE;
+            case 32:
+                return Color.MEDIUMSEAGREEN;
+            case 64:
+                return Color.LIMEGREEN;
+            case 128:
+                return Color.DARKORANGE;
+            case 256:
+                return Color.SALMON;
+            case 512:
+                return Color.ORANGERED;
+            case 1024:
+                return Color.DEEPPINK;
+            case 2048:
+                return Color.MEDIUMVIOLETRED;
+            default:
+                return Color.NONE;
+        }
+    }
+
     private void drawScene() {
         for (int y = 0; y < SIDE; y++) {
             for (int x = 0; x < SIDE; x++) {
-                setCellColor(x, y, Color.GREEN);
+                setCellColoredNumber(x, y, gameField[y][x]);
             }
         }
     }
