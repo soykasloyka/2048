f90d614979fe41887523a0f6d776f0f3aee29d19
Soyka <petr.emelianov@icloud.com>
2023-02-13 09:29:13 +0200

    game restart

--- Game2048.java
+++ Game2048.java
@@ -16,6 +16,16 @@ public class Game2048 extends Game {
 
     @Override
     public void onKeyPress(Key key) {
+        if (isGameStopped) {
+            if (key == Key.SPACE) {
+                isGameStopped = false;
+                createGame();
+                drawScene();
+            } else {
+                return;
+            }
+        }
+
         if (!canUserMove()) {
             gameOver();
             return;
@@ -36,6 +46,7 @@ public class Game2048 extends Game {
     }
 
     private void createGame() {
+        gameField = new int[SIDE][SIDE];
         createNewNumber();
         createNewNumber();
     }
