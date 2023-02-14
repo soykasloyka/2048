aaa2d396f40ad99f7752bb83508a3ab7f846d569
Soyka <petr.emelianov@icloud.com>
2023-02-14 11:28:50 +0200

    .

--- Game2048.java
+++ Game2048.java
@@ -3,7 +3,7 @@ package com.javarush.games.game2048;
 import com.javarush.engine.cell.*;
 
 public class Game2048 extends Game {
-    private static final int SIDE = 4;
+    private static final int SIDE = 5;
     private int[][] gameField = new int[SIDE][SIDE];
     private boolean isGameStopped = false;
     private int score = 0;
